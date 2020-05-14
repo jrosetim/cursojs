694cdf34a019979869fdf8305f92b8bb785b091c
jrosetim <jrosetim@gmail.com>
2020-05-14 20:48:16 -0300

    curso js es6

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// import ClasseUsuario from './functions';
+// import {idade} from './functions';
+
+// ClasseUsuario.info();
+// console.log(idade);
+
+//---------------------------------------------------------------
+import Usuario, {idade as IdadeUsuario} from './functions';
+
+Usuario.info();
+console.log(IdadeUsuario);
+
+//---------------------------------------------------------------
+
+
